888b0daf32df2b21dcdc99aa14670cbe20e4fb8b
cmcsoperco <cmcsoperco@gmail.com>
2024-06-27 14:38:10 +0530

    Create test_app_1.py

---
+++ test_app_1.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import streamlit as st
+st.set_page_config(page_title="Field Wise File Maker", page_icon=":bar_chart:", layout="wide")
+st.subheader(":green[This site has been developed by]:red[ Rajib Mondal(Manager-IT, CCD)]")
+#st.image(image="cbi_logo.png")
+st.warning(":red[This is my first app!!!]", icon="⚠️")
